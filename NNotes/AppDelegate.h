0b171e1ab68623675060a9c62ece649cc5b002ef
Olga Vyrostko <vyrostkoolga@gmail.com>
2016-06-13 18:00:13 +0500

    initial commit

---
+++ NNotes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NNotes
+//
+//  Created by Ольга Выростко on 12.06.16.
+//  Copyright © 2016 Ольга Выростко. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NNotes/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NNotes
+//
+//  Created by Ольга Выростко on 12.06.16.
+//  Copyright © 2016 Ольга Выростко. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
